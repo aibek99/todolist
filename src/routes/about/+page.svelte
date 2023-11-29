a751f8c96611ace2917275dfcac4556b475c3c69
aibek.bakirov <aibek.bakirov99@gmail.com>
2023-11-29 09:40:53 +0300

    update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,13 +6,12 @@
 		<p>
 			Welcome to Assignment project, a website designed to help you keep track of your assignments!
 			This project is a simple CMS that mimics the features of a CRUD system, enabling users to
-			create, read, update
+			create, read, update and delete.
 		</p>
 
 		<p>
 			I used the latest technologies to implement this project, including SvelteKit with Javascript,
-			ESLint, and Prettier for the frontend, and firebase for the backend. This ensures that website
-			is not only user
+			ESLint, and Prettier for the frontend, and firebase for the backend.
 		</p>
 		<p>
 			In conclusion, Assignment tool is designed to make it easy for you to manage your assignments.
